641865feafe8bd9b4c823f5ae94cbbe3218391dd
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-17 16:52:55 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -126,7 +126,7 @@ $conn->close();
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="addOrdersLabel">Add Customer</h1>
+              <h1 class="modal-title fs-5" id="addOrdersLabel">Add Order</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
