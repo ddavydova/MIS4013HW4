16490f53f5e430c2918b161ec6c56625d1b00f89
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-17 17:15:27 -0500

    Update suppliers.php

--- suppliers.php
+++ suppliers.php
@@ -73,7 +73,7 @@ if ($result->num_rows > 0) {
               <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editSupplier<?=$row["supplier_id"]?>">
                 Edit
               </button>
-              <div class="modal fade" id="editSupplier<?=$row["supplier_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editProduct<?=$row["supplier_id"]?>Label" aria-hidden="true">
+              <div class="modal fade" id="editSupplier<?=$row["supplier_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editSupplier<?=$row["supplier_id"]?>Label" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
