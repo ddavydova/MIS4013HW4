2233550ac3ab00b69526c4ec59f81badea0b9e60
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-17 16:29:36 -0500

    Update links.php

--- links.php
+++ links.php
@@ -1,13 +1,16 @@
 <div class="container display-4">
 	<ul class="nav nav-pills nav-fill">
           <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="index.php">Home</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="index.php">Home (Customers)</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="page3.php">Cards View</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="orders.php">Orders Modification</a>
 	  </li>
 	<li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="page4.php">Post Page</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="products.php">Products Modification</a>
+	  </li>
+	<li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Main Page") {?>active<?php }?>" href="suppliers.php">Suppliers Modification</a>
 	  </li>
 	</ul>
 </div>
