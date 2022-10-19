74d6d187c1cfd9c3f887ff97d8c9262b22ad9f3c
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-19 15:28:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -96,19 +96,9 @@ if ($result->num_rows > 0) {
               </div>
             </td>
             <td>
-              <form method="post" action="">
+             <form method="post" action="">
                 <input type="hidden" name="iid" value="<?=$row["customer_id"]?>" />
                <select class="form-select" aria-label="Select instructor" id="instructorList" name="iid">
-<?php
-    $instructorSql = "select * from instructor order by instructor_name";
-    $instructorResult = $conn->query($instructorSql);
-    while($instructorRow = $instructorResult->fetch_assoc()) {
-      if ($instructorRow['instructor_id'] == $row['instructor_id']) {
-        $selText = " selected";
-      } else {
-        $selText = "";
-      }
-?>
                 <input type="hidden" name="saveType" value="Delete">
                 <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
               </form>
