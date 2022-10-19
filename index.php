07355d08c0e99130ee1aea6a105a4ec5d5455acd
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-18 20:05:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into Customer (fname) value ('$custName')";
+      $sqlAdd = "insert into Customer (fname) value (?)";
       $stmtAdd = $conn->prepare($sqlAdd);
       $stmtAdd->bind_param("s", $_POST['custName']);
       $stmtAdd->execute();
