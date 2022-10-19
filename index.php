e2f9503aa5fbeb22c984c55e42c99727524db693
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-18 20:00:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -150,33 +150,10 @@ $conn->close();
 		<form action="insert.php" method="post">
 			
 <p>
-			<label for="firstName">First Name:</label>
-			<input type="text" name="first_name" id="firstName">
+			<label for="fname"> Name:</label>
+			<input type="text" name="fname" id="fname">
 			</p>
-
 			
-<p>
-			<label for="lastName">Last Name:</label>
-			<input type="text" name="last_name" id="lastName">
-			</p>
-
-			
-<p>
-			<label for="Gender">Gender:</label>
-			<input type="text" name="gender" id="Gender">
-			</p>
-
-			
-<p>
-			<label for="Address">Address:</label>
-			<input type="text" name="address" id="Address">
-			</p>
-
-			
-<p>
-			<label for="emailAddress">Email Address:</label>
-			<input type="text" name="email" id="emailAddress">
-			</p>
 
 			<input type="submit" value="Submit">
 		</form>
