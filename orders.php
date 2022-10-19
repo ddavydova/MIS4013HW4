a17d4029d8d442df063bf221b58461c3a68d0191
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-18 19:15:53 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -68,7 +68,7 @@ if ($result->num_rows > 0) {
           
           <tr>
             <td><?=$row["order_id"]?></td>
-            
+            <td><?=$row["quantity"]?></td>
             <td>
               <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editOrders<?=$row["order_id"]?>">
                 Edit
