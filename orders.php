0e8b62c094862f4a665b0b2b179ad5ff9aef7d49
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-20 11:02:02 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -43,33 +43,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       break;
   }
 }
-     $sql = "SELECT * FROM `Orders`";
-    // The following code checks if the submit button is clicked
-    // and inserts the data in the database accordingly
-    if(isset($_POST['submit']))
-    {
-        // Store the Product name in a "name" variable
-        $name = mysqli_real_escape_string($con,$_POST['orderquantity']);
-        
-        // Store the Category ID in a "id" variable
-        $id = mysqli_real_escape_string($con,$_POST['Category']);
-        
-        // Creating an insert query using SQL syntax and
-        // storing it in a variable.
-        $sql_insert =
-        "INSERT INTO `Orders`(`quantity`, `customer_id`)
-            VALUES ('$name','$id')";
-          
-          // The following code attempts to execute the SQL query
-          // if the query executes with no errors
-          // a javascript alert message is displayed
-          // which says the data is inserted successfully
-          if(mysqli_query($con,$sql_insert))
-        {
-            echo '<script>alert("Product added successfully")</script>';
-        }
-    }
-?>
     
       <table class="table table-striped">
         <thead>
