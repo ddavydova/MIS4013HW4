96bfc8343a37bbeaa1e2c25ad8cac7313208c619
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-20 15:41:16 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -64,6 +64,43 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
+         
+<?php
+         // mysqli_connect("servername","username","password","database_name")
+  
+    // Get all the categories from category table
+    $sql = "SELECT * FROM `Customer`";
+    $all_categories = mysqli_query($con,$sql);
+  
+    // The following code checks if the submit button is clicked
+    // and inserts the data in the database accordingly
+    if(isset($_POST['submit']))
+    {
+        // Store the Product name in a "name" variable
+        $name = mysqli_real_escape_string($con,$_POST['fname']);
+        
+        // Store the Category ID in a "id" variable
+        $id = mysqli_real_escape_string($con,$_POST['customer_id']);
+      
+        $quantity = mysqli_real_escape_string($con,$_POST['quantity']);
+        $pid = mysqli_real_escape_string($con,$_POST['product_id']);
+        
+        // Creating an insert query using SQL syntax and
+        // storing it in a variable.
+        $sql_insert =
+        "INSERT INTO `orders`(`customer_id`, `product_id`, 'quantity', )
+            VALUES ('$name','$id', '$quantity', '$pid')";
+          
+          // The following code attempts to execute the SQL query
+          // if the query executes with no errors
+          // a javascript alert message is displayed
+          // which says the data is inserted successfully
+          if(mysqli_query($con,$sql_insert))
+        {
+            echo '<script>alert("Order added successfully")</script>';
+        }
+    }
+?>
           
           <tr>
             <td><?=$row["order_id"]?></td>
@@ -139,13 +176,14 @@ $conn->close();
                 while ($category = mysqli_fetch_array(
                         $all_categories,MYSQLI_ASSOC)):;
             ?>
-                <option value="<?php echo $category["Category_ID"];
+                <option value="<?php echo $category["customer_id"];
                     // The value we usually set is the primary key
                 ?>">
-                    <?php echo $category["Category_Name"];
+                    <?php echo $category["fname"];
                         // To show the category name to the user
                     ?>
                 </option>
+            
             <?php
                 endwhile;
                 // While loop must be terminated
