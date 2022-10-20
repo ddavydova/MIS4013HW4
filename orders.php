44b0364faaf3789329be59ee83239120f42352f5
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-20 15:44:57 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -13,7 +13,7 @@ $password = "dasha12345!";
 $dbname = "davyddov_HW3";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
