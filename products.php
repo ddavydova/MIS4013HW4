4d28c107043a9de5c8839bbc9444cdba07fb2649
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-24 13:02:38 -0500

    Update products.php

--- products.php
+++ products.php
@@ -121,8 +121,8 @@ $conn->close();
         Add New
       </button>
 
-      <!-- Modal -->
-      <div class="modal fade" id="addProduct" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addProductLabel" aria-hidden="true">
+      <!-- Modal -->   
+   div class="modal fade" id="addProduct" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addProductLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
@@ -130,23 +130,25 @@ $conn->close();
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <form method="post" action="">
-               <div class="mb-3">
-                  <label for="pname" class="form-label">Enter the Product Name: </label>
-                  <input type="text" class="form-control" id="pname" aria-describedby="nameHelp" name="iName">
-                </div>
-               <div class="mb-3">
+             <form method="post" action="">
+              <div class="mb-3">
+               <label for="pname" class="form-label">Enter the Product Name:</label>
+               <input type="text" class="form-control"  aria-describedby="nameHelp" name="iName" required><br>
+              </div>
+              <div class="mb-3">
                  <label for="sname" class="form-label">Pick the Supplier</label>
-                 <select class="form-select" aria-label="Select Supplier" id="sname" name="iid"> 
-                </select>
+                 <select class="form-select" aria-label="Select product" id="sname" name="iid"></select>
                </div>
-                <input type="hidden" name="saveType" value="Add">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-             </div>
+                 <input type="hidden" name="saveType" value="Add">
+                 <button type="submit" class="btn btn-primary">Submit</button>
+           </form>
+            </div>
           </div>
-        </div>
-      </div>
+          </div>
+          </div>
+     </div>
+ 
+ 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <?php include("footer.php");?>
   </body>
